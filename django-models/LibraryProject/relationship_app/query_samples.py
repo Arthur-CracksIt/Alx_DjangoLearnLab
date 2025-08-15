b602add6885cfe0ec14d93bf53e184a7d678f897
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-08-15 23:24:32 +0000

    author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,16 @@
-from relationship_app.models import Book, Library
+from relationship_app.models import Book, Library, Author
+#Get all books
 book = Book.objects.all()
+
+#Get books in library
 library_name = "Balm Library"
 library = Library.objects.get(name=library_name)
 books = library.books.all()
 print(f"Books in {library_name}:" )
 for book in books:
     print(book.title)
+
+#Get book by author
+author_name = 'David'
+author = Author.objects.get(name = author_name)
+books = Book.objects.filter(author =author)
