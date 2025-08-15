7f600e8cf77568b088db7cc5e246412aea28977e
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-08-15 23:16:30 +0000

    checks

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,3 +1,8 @@
 from relationship_app.models import Book, Library
 book = Book.objects.all()
+library_name = "Balm Library"
 library = Library.objects.get(name='library_name')
+books = library.books.all()
+print(f"Books in {library_name}:" )
+for book in books:
+    print(book.title)
