80090fe9fa325a63d1a8b0f7e50b40fc5f829fbc
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-08-21 20:41:45 +0000

    added the int pk to check

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,5 +3,5 @@ from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/',list_books, name= 'ListBooks'),
-    path('library/', LibraryDetailView.as_view(), name= 'library_details')
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name= 'library_details')
 ]
