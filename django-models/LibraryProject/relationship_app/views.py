b9165c061fb0b66adbc6ecabeb0889756cde40fc
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-08-26 22:14:23 +0000

    logout and login again

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,7 +18,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 class Register(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'templates/relationship_app/register.html'
     def form_valid(self, form):
