c16447afb73114b5372298c9108c89df4a0cd3fb
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-09-07 19:14:21 +0000

    create Admin for custom user

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
 # Register your models here.
 class BookAdmin(admin.ModelAdmin): #customizing the admin
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'publication_year')
 admin.site.register(Book, BookAdmin)
+
+class CustomeUserAdmin(admin.ModelAdmin):
+    user_display = ('profile_photo', 'date_of_birth')
+admin.site.register(CustomUser, CustomeUserAdmin)
