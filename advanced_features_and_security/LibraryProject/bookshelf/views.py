1a0a16fef9786bd8b1364fe0ee200d2cc8745312
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-09-08 22:13:02 +0000

    check

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -12,17 +12,17 @@ class SignUpView(CreateView):
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def can_edit(request):
-    edit_role= UserPermissions.objects.create(role= 'Editor')
+    edit_role= UserPermissions.objects.create(name= 'Editor')
     return render(request, 'relationship_app/editors.html', {'user': edit_role})
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def can_delete(request):
-    view_role= UserPermissions.objects.create(role= 'Viewer')
+    view_role= UserPermissions.objects.create(name= 'Viewer')
     return render(request, 'relationship_app/viewer.html', {'user': view_role})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def can_change_book(request, name):
-    delete_role = UserPermissions.objects.get(name = name)
+    delete_role = UserPermissions.objects.get(name = 'Admin')
     delete_role.delete()
     return render(request, 'templates/relationship_app/change_book.html', {'book': delete_role})
 
